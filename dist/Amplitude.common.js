01321255f95ceb2dd4b6018621bbd5ca397bd471
mParticle Automation <developers@mparticle.com>
2020-06-03 16:07:52 +0000

    Generate latest bundle

--- dist/Amplitude.common.js
+++ dist/Amplitude.common.js
@@ -11,16 +11,6 @@ function isObject(val) {
   return val != null && typeof val === 'object' && Array.isArray(val) === false;
 }
 
-var isobject = /*#__PURE__*/Object.freeze({
-  'default': isObject
-});
-
-function getCjsExportFromNamespace (n) {
-	return n && n['default'] || n;
-}
-
-var isobject$1 = getCjsExportFromNamespace(isobject);
-
 /* eslint-disable no-undef*/
 //
 //  Copyright 2015 mParticle, Inc.
@@ -53,6 +43,34 @@ var isobject$1 = getCjsExportFromNamespace(isobject);
 
     var constants = {
         MPID: 'mpId'
+    };
+
+    var renderSnippet = function () {
+        /* eslint-disable */
+        (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script")
+        ;r.type="text/javascript"
+        ;r.integrity="sha384-RsEu4WZflrqYcEacpfoGSib3qaSvdYwT4D+DrWqeBuDarSzjwUQR1jO8gDiXZd0E"
+        ;r.crossOrigin="anonymous";r.async=true
+        ;r.src="https://cdn.amplitude.com/libs/amplitude-6.2.0-min.gz.js"
+        ;r.onload=function(){if(!e.amplitude.runQueuedFunctions){
+        console.log("[Amplitude] Error: could not load SDK");}}
+        ;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
+        ;function s(e,t){e.prototype[t]=function(){
+        this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));return this};}
+        var o=function(){this._q=[];return this}
+        ;var a=["add","append","clearAll","prepend","set","setOnce","unset"]
+        ;for(var u=0;u<a.length;u++){s(o,a[u]);}n.Identify=o;var c=function(){this._q=[]
+        ;return this}
+        ;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
+        ;for(var p=0;p<l.length;p++){s(c,l[p]);}n.Revenue=c
+        ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","enableTracking","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
+        ;function v(e){function t(t){e[t]=function(){
+        e._q.push([t].concat(Array.prototype.slice.call(arguments,0)));};}
+        for(var n=0;n<d.length;n++){t(d[n]);}}v(n);n.getInstance=function(e){
+        e=(!e||e.length===0?"$default_instance":e).toLowerCase()
+        ;if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e]);}return n._iq[e]}
+        ;e.amplitude=n;})(window,document);
+        /* eslint-enable */
     };
 
     var constructor = function() {
@@ -259,26 +277,7 @@ var isobject$1 = getCjsExportFromNamespace(isobject);
             try {
                 if (!window.amplitude) {
                     if (testMode !== true) {
-                        /* eslint-disable */
-                        (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script");r.type="text/javascript"
-                            ;r.async=true;r.src="https://cdn.amplitude.com/libs/amplitude-4.2.1-min.gz.js"
-                            ;r.onload=function(){if(e.amplitude.runQueuedFunctions){e.amplitude.runQueuedFunctions();}else{console.log("[Amplitude] Error: could not load SDK");}}
-                            ;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i);function s(e,t){e.prototype[t]=function(){
-                            this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));return this};}
-                            var o=function(){this._q=[];return this}
-                            ;var a=["add","append","clearAll","prepend","set","setOnce","unset"]
-                            ;for(var u=0;u<a.length;u++){s(o,a[u]);}n.Identify=o;var c=function(){this._q=[]
-                            ;return this}
-                            ;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
-                            ;for(var p=0;p<l.length;p++){s(c,l[p]);}n.Revenue=c
-                            ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
-                            ;function v(e){function t(t){e[t]=function(){
-                            e._q.push([t].concat(Array.prototype.slice.call(arguments,0)));};}
-                            for(var n=0;n<d.length;n++){t(d[n]);}}v(n);n.getInstance=function(e){
-                            e=(!e||e.length===0?"$default_instance":e).toLowerCase()
-                            ;if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e]);}return n._iq[e]}
-                            ;e.amplitude=n;})(window,document);
-                        /* eslint-enable */
+                        renderSnippet();
                     }
                 }
 
@@ -355,12 +354,12 @@ var isobject$1 = getCjsExportFromNamespace(isobject);
             return;
         }
 
-        if (!isobject$1(config)) {
+        if (!isObject(config)) {
             window.console.log('\'config\' must be an object. You passed in a ' + typeof config);
             return;
         }
         
-        if (isobject$1(config.kits)) {
+        if (isObject(config.kits)) {
             config.kits[name] = {
                 constructor: constructor
             };
