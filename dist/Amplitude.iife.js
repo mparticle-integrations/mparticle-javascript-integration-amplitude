2ad78ca28c06536db815f48a2bb1b5b5a08c2e80
Robert Ing <ring@mparticle.com>
2021-01-11 14:45:09 -0500

    Generate latest bundle

--- dist/Amplitude.iife.js
+++ dist/Amplitude.iife.js
@@ -1,14 +1,13 @@
 var mpAmplitudeKit = (function (exports) {
-  /*!
-   * isobject <https://github.com/jonschlinkert/isobject>
-   *
-   * Copyright (c) 2014-2017, Jon Schlinkert.
-   * Released under the MIT License.
-   */
+  var toString = {}.toString;
 
-  function isObject(val) {
-    return val != null && typeof val === 'object' && Array.isArray(val) === false;
-  }
+  var isarray = Array.isArray || function (arr) {
+    return toString.call(arr) == '[object Array]';
+  };
+
+  var isobject = function isObject(val) {
+    return val != null && typeof val === 'object' && isarray(val) === false;
+  };
 
   /* eslint-disable no-undef*/
   //
@@ -48,9 +47,9 @@ var mpAmplitudeKit = (function (exports) {
           /* eslint-disable */
           (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script")
           ;r.type="text/javascript"
-          ;r.integrity="sha384-RsEu4WZflrqYcEacpfoGSib3qaSvdYwT4D+DrWqeBuDarSzjwUQR1jO8gDiXZd0E"
+          ;r.integrity="sha384-girahbTbYZ9tT03PWWj0mEVgyxtZoyDF9KVZdL+R53PP5wCY0PiVUKq0jeRlMx9M"
           ;r.crossOrigin="anonymous";r.async=true
-          ;r.src="https://cdn.amplitude.com/libs/amplitude-6.2.0-min.gz.js"
+          ;r.src="https://cdn.amplitude.com/libs/amplitude-7.2.1-min.gz.js"
           ;r.onload=function(){if(!e.amplitude.runQueuedFunctions){
           console.log("[Amplitude] Error: could not load SDK");}}
           ;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
@@ -58,10 +57,10 @@ var mpAmplitudeKit = (function (exports) {
           this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));return this};}
           var o=function(){this._q=[];return this}
           ;var a=["add","append","clearAll","prepend","set","setOnce","unset"]
-          ;for(var u=0;u<a.length;u++){s(o,a[u]);}n.Identify=o;var c=function(){this._q=[]
+          ;for(var c=0;c<a.length;c++){s(o,a[c]);}n.Identify=o;var u=function(){this._q=[]
           ;return this}
           ;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
-          ;for(var p=0;p<l.length;p++){s(c,l[p]);}n.Revenue=c
+          ;for(var p=0;p<l.length;p++){s(u,l[p]);}n.Revenue=u
           ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","enableTracking","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
           ;function v(e){function t(t){e[t]=function(){
           e._q.push([t].concat(Array.prototype.slice.call(arguments,0)));};}
@@ -372,16 +371,16 @@ var mpAmplitudeKit = (function (exports) {
 
       function register(config) {
           if (!config) {
-              window.console.log('You must pass a config object to register the kit ' + name);
+              console.log('You must pass a config object to register the kit ' + name);
               return;
           }
 
-          if (!isObject(config)) {
-              window.console.log('\'config\' must be an object. You passed in a ' + typeof config);
+          if (!isobject(config)) {
+              console.log('\'config\' must be an object. You passed in a ' + typeof config);
               return;
           }
           
-          if (isObject(config.kits)) {
+          if (isobject(config.kits)) {
               config.kits[name] = {
                   constructor: constructor
               };
@@ -391,17 +390,18 @@ var mpAmplitudeKit = (function (exports) {
                   constructor: constructor
               };
           }
-          window.console.log('Successfully registered ' + name + ' to your mParticle configuration');
+          console.log('Successfully registered ' + name + ' to your mParticle configuration');
       }
 
-      if (window && window.mParticle && window.mParticle.addForwarder) {
-          window.mParticle.addForwarder({
-              name: name,
-              constructor: constructor,
-              getId: getId
-          });
+      if (typeof window !== 'undefined') {
+          if (window && window.mParticle && window.mParticle.addForwarder) {
+              window.mParticle.addForwarder({
+                  name: name,
+                  constructor: constructor,
+                  getId: getId
+              });
+          }
       }
-
       var Amplitude = {
           register: register
       };
