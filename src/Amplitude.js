f7ad2bbedb40bbc1a76337de474c06626e3deb91
Robert Ing <robertming@gmail.com>
2021-01-11 14:40:56 -0500

    Update Amplitude SDK to 7.2.1

--- src/Amplitude.js
+++ src/Amplitude.js
@@ -36,9 +36,9 @@
         /* eslint-disable */
         (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script")
         ;r.type="text/javascript"
-        ;r.integrity="sha384-RsEu4WZflrqYcEacpfoGSib3qaSvdYwT4D+DrWqeBuDarSzjwUQR1jO8gDiXZd0E"
+        ;r.integrity="sha384-girahbTbYZ9tT03PWWj0mEVgyxtZoyDF9KVZdL+R53PP5wCY0PiVUKq0jeRlMx9M"
         ;r.crossOrigin="anonymous";r.async=true
-        ;r.src="https://cdn.amplitude.com/libs/amplitude-6.2.0-min.gz.js"
+        ;r.src="https://cdn.amplitude.com/libs/amplitude-7.2.1-min.gz.js"
         ;r.onload=function(){if(!e.amplitude.runQueuedFunctions){
         console.log("[Amplitude] Error: could not load SDK")}}
         ;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
@@ -46,10 +46,10 @@
         this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));return this}}
         var o=function(){this._q=[];return this}
         ;var a=["add","append","clearAll","prepend","set","setOnce","unset"]
-        ;for(var u=0;u<a.length;u++){s(o,a[u])}n.Identify=o;var c=function(){this._q=[]
+        ;for(var c=0;c<a.length;c++){s(o,a[c])}n.Identify=o;var u=function(){this._q=[]
         ;return this}
         ;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
-        ;for(var p=0;p<l.length;p++){s(c,l[p])}n.Revenue=c
+        ;for(var p=0;p<l.length;p++){s(u,l[p])}n.Revenue=u
         ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","enableTracking","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
         ;function v(e){function t(t){e[t]=function(){
         e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}
